f1a85450b0db38595d1d8d72f66ecf737d41d837
powehihihi <110171941+powehihihi@users.noreply.github.com>
2023-03-11 10:32:10 +0300

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@
 #include <iostream>
 
 int main() {
-  Song s1 = {"SHTRIHCOD - ARLONG PARK", 114}, s2 = {"pfctlord - #3", 226};
-  App Myapp(std::list<Song>{s1, s2}, -1);
+  Song s1 = {"NewJeans - Ditto", 186}, s2 = {"blackbear - @ my worst", 191},
+       s3 = {"347aidan - MEMORIES!", 138};
+  App Myapp(std::list<Song>{s1, s2, s3}, -1);
   Myapp.Run();
   return 0;
 }
